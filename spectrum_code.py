f27a02bb9b759848555b00aa6d1fd793f9cbc333
MariaClaramunt <mariaclaramuntclotet@gmail.com>
2025-06-13 18:34:05 +0200

    Add files via upload

--- spectrum_code.py
+++ spectrum_code.py
@@ -7,23 +7,23 @@ c1=c+dc
 c2=c-dc
 L=10/c
 
-k_vals= np.linspace(0, 2*np.pi+0.2, 100)
+q_vals=np.linspace(0, 2*np.pi+0.2, 100)
 
 # SSH HAMILTONIAN
-def ssh_hamiltonian(k, c, dc):
+def ssh_hamiltonian(q, c, dc):
     c1=c+dc
     c2=c-dc
     H = np.array([
-        [0, c1 + c2 * np.exp(-1j * k)],
-        [c1 + c2 * np.exp(1j * k), 0]
+        [0, c1 + c2 * np.exp(-1j * q)],
+        [c1 + c2 * np.exp(1j * q), 0]
         ])
     eigenvals, eigenvecs = np.linalg.eigh(H)  # eigenvalues and eigenvectors in ascending order and repeated according to multiplicity
     return eigenvals, eigenvecs
 
 # COMPUTATION OF THE EIGENVALUES
 all_eigenvals = []
-for k in k_vals:
-    eigenvals, _ = ssh_hamiltonian(k, c, dc)
+for q in q_vals:
+    eigenvals, _ = ssh_hamiltonian(q, c, dc)
     all_eigenvals.append(eigenvals)
 all_eigenvals = np.array(all_eigenvals) # list to array
 
@@ -32,12 +32,13 @@ fontsize= 30
 linewidth = 3
 
 plt.figure(figsize=(7,5))
-plt.plot(k_vals, 2 * c * np.cos(k_vals / 2), color='blue', linewidth = linewidth) # dc=0 case
-plt.plot(k_vals[k_vals>np.pi], all_eigenvals[:, 0][k_vals>np.pi], color='red', linewidth = linewidth)
-plt.plot(k_vals[k_vals<np.pi], all_eigenvals[:, 1][k_vals<np.pi], color='red', linewidth = linewidth)
+plt.plot(q_vals, 2 * c * np.cos(q_vals / 2), color='blue', linewidth = linewidth) # dc=0 case
+plt.plot(q_vals[q_vals>np.pi], all_eigenvals[:, 0][q_vals>np.pi], color='red', linewidth = linewidth)
+plt.plot(q_vals[q_vals<np.pi], all_eigenvals[:, 1][q_vals<np.pi], color='red', linewidth = linewidth)
 
 plt.xlabel(r'$q$', fontsize=fontsize)
-plt.ylabel(r'$\beta$', rotation=0,fontsize=fontsize)
+ylabel=plt.ylabel(r'$\beta$', rotation=0,fontsize=fontsize)
+ylabel.set_position((0, 0.42))
 plt.xlim(0, 2*np.pi+0.2)
 plt.ylim(-2, 2.2)
 plt.xticks([ 0, np.pi, 2*np.pi], [r'$0$', r'$\pi$', r'$2\pi$'], fontsize=fontsize)
